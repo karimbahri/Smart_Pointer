2120026e378f9127ca1532294777d2048b752d1a
karim bahri <karim-bahri-18@outlook.fr>
2020-07-01 23:13:36 +0000

    add smart_ptr header

---
+++ Smart_Ptr.h
@@ -0,0 +1,15 @@
+#ifndef __SMART_PTR__
+#define __SMART_PTR__
+
+#include "lists.h"
+
+/*--------------------------STRUCTS--------------------------*/
+
+typedef struct
+{
+	listint_t *stack;
+}Smart_Ptr;
+
+
+/*--------------------------STRUCTS--------------------------*/
+#endif
